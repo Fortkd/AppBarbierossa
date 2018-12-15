d84c3f40a64afee36c998915907ab6382e18f911
fortkd <polverinofortu@gmail.com>
2018-12-15 01:29:44 +0100

    v0.2.9 (attivazione NavigationBar con relative classi)

--- app/src/main/java/com/example/fortu/appbarbierossa/MainActivity.java
+++ app/src/main/java/com/example/fortu/appbarbierossa/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.fortu.appbarbierossa;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private TextView mTextMessage;
+    /*private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -29,16 +32,42 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         }
-    };
+    };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+       // mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
     }
 
+    private boolean loadFragment(Fragment fragament){
+        if(fragament != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragament).commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.navigation_dashboard:
+                fragment = new PrenotaFragment();
+                break;
+            case R.id.navigation_notifications:
+                fragment = new NotificationFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
 }
